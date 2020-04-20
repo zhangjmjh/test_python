1d20fedf8ff5462988b8843b7c586093f9802155
zhangjmjh <zhangjm.jh@qq.com>
2020-04-20 18:37:10 +0800

    这个文件是放一些python的基础语发

---
+++ 02_python.py
@@ -0,0 +1,11 @@
+# list
+classname = ['mike','jerry','bob']
+print(classname[0]) # 读取列表中的值，括号中为对应列表中的索引
+
+classname = ['mike','jerry','bob']
+classname[0]= 'hehe'  # 更新列表中的数据，括号中为对应列表中的索引
+print(classname)
+
+classname = ['mike','jerry','bob']
+del classname[0]
+print(classname)   # 删除列表元素
