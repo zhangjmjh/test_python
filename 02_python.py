f378bd7772e334229316cb0b67fc8d60a9f078db
zhangjmjh <zhangjm.jh@qq.com>
2020-04-23 17:22:53 +0800

    我学习了一些return的语发

--- 02_python.py
+++ 02_python.py
@@ -18,11 +18,15 @@
 # print_welcome('jerry')   #  我调用了这个print_welcome的函数，括号里是我给他传的参数，jerry ,最后就打印出了这个结果
 
 # 函数2
-def area(width, height):
-    return width * height
-w = 4
-h = 8
+# def area(width, height):
+#     return width * height
+# w = 4
+# h = 8
 # print('这是我算出来的面积：',area(w , h ))  # area(w, h) 可以看做是一个整体，可以理解为一个变量a，a = area(w, h),
 # 最终的结果就是：print(a)，所以可以写成上面的写法 print('这是我算出来的面积：',a)  由于函数体不是直接print 而是用的是return
 # return的作用是运行加减乘除的,最后在一起打印出来
 
+# return的用法  return是返回的意思 print 是打印结果的意思
+# def plus(a,b):  # 自定义一个 plus的函数，必须要传2个参数 a 和 b
+# #     return a+b   # reutrn 返回出 a+b的结果   但是此时是不会打印出来
+# # print(plus(2,6))  #  这里才是打印出来
---
+++ 03_test_unitest.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+import unittest, time
+
+# 四大组件
+# test fixture  setUp  tearDown
+#test case 测试用例 通过集成unitest TestCase 来实现用例的继承 在UniTest中，测试用例都是通过test来识别的
+# test suite 测试套件 测试用例集
+# test runner  运行器 一般通过runner来调用suit去执行测试
+#unitest 运行机制 通过main函数中，调用unitest main()运行所有的内容
+
+class BaiduTest(unittest.TestCase):  #通过继承unitest,TestCase来实现用例
+    def setup(self):
+        self.driver = webdriver.Chrome()
+        self.driver.implicitly_wait(20)
+        self.driver.get('http://www.baidu.com')
+
+
+    # 函数
+    def plus(self, a ,b):
+        c = a+b
+        print(c)
+        return c
+    # 测试用例，加test才会认为你这个是测试用例
+    def test_baidu(self):
+        pass
+    def test_youdao(self):
+        pass
+    def test_a(self):
+        self.plus(1, 2)
+        print('c')
+
+    def tearDown(self):
+
+if __name__ == '__main__':
+    unittest.main()
