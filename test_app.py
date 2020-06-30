7f4e8b86458200944c79e829d70556d7c9feb36a
zhangjmjh <zhangjm.jh@qq.com>
2020-06-30 19:06:13 +0800

    zfx 自动化脚本

--- test_app.py
+++ test_app.py
@@ -1,13 +1,11 @@
 from appium import webdriver
 import pytest
-# import unittest
 from zfx_app_element import is_element_present
-# from appium.common.exceptions import NoSuchElementException
-# import time
+import time
 
 
 class TestSearch():
-    def setup(self):
+    def setup(self):     #  初始化整个APP运行环境
         desired_caps = {
             'platformName': 'Android',
             'deviceName': '7XBRX18B15002828',
@@ -15,41 +13,116 @@ class TestSearch():
             'appPackage': 'com.shenzhen.yirabbit',
             'appActivity': 'com.shenzhen.yirabbit.ui.activity.WelComeActivity',
             'newCommandTimeout': 2000,
-            'automationName': 'UiAutomator2'
+            'automationName': 'UiAutomator2',
+            'noReset': True
         }
         self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-        self.driver.find_element_by_id('com.android.permissioncontroller:id/permission_allow_button').click()
-        self.driver.find_element_by_id('com.android.permissioncontroller:id/permission_allow_always_button').click()
-        self.driver.find_element_by_id('com.android.permissioncontroller:id/permission_allow_button').click()
-        self.driver.find_element_by_id('com.android.permissioncontroller:id/permission_allow_button').click()
-        self.driver.implicitly_wait(10)
-        self.driver.find_element_by_id('com.shenzhen.yirabbit:id/confirm').click()
-        self.driver.implicitly_wait(10)
-        self.driver.find_element_by_id('com.shenzhen.yirabbit:id/closeImage').click()
         self.driver.implicitly_wait(10)
 
-    def test_login(self):  #  登陆
-        self.driver.find_element_by_id('com.shenzhen.yirabbit:id/priceRadioButton').click()   # 协议
-        self.driver.find_element_by_id("com.shenzhen.yirabbit:id/loginButton").click()  # 弹窗
-        self.driver.find_element_by_id("com.shenzhen.yirabbit:id/account").send_keys('18800000000')
-        self.driver.find_element_by_id("com.shenzhen.yirabbit:id/password").send_keys('Qo123456')
-        self.driver.find_element_by_id("com.shenzhen.yirabbit:id/finishButton").click()  #登陆
-        self.driver.implicitly_wait(10)
-        self.driver.find_element_by_id("com.shenzhen.yirabbit:id/closeImage").click()  #   关闭活动弹窗
-        #self.driver.find_element_by_id("com.shenzhen.yirabbit:id/priceRadioButton").click()    #  交易
-        #self.driver.find_element_by_id('com.shenzhen.yirabbit:id/sureOpenButton').click()  #  新手指引(知道了)
-        # self.driver.find_element_by_id('com.shenzhen.yirabbit:id/sureOpenButton')  #  断言出现新手指引登陆成功
-        is_element_present(self.driver, 'id', 'com.shenzhen.yirabbit:id/haveNotic')
+    # def test_login(self):  #  判断登陆是否成功
+    #
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/closeImage").click()  # 关闭活动弹窗
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/priceRadioButton").click()  # 交易
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/loginButton").click()  # 登陆
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/account").clear()
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/account").send_keys('18800000000')
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/password").clear()
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/password").send_keys('Qo123456')
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/finishButton").click()  #登陆
+    #     self.driver.implicitly_wait(15)
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/closeImage").click()  #   关闭活动弹窗
+    #     is_element_present(self.driver, 'id', 'com.shenzhen.yirabbit:id/haveNotice')
 
-    # def test_transaction(self):  # 投票
-    #     pass
+    # def test_market_buy(self):
+    #     """
+    #     市价买入澳元兑日元后平仓
+    #     :return:
+    #     """
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/closeImage").click()  # 关闭活动弹窗
+    #     self.driver.implicitly_wait(10)
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/priceRadioButton").click()    #  交易
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/placeOrderButton").click()  #  去下单
+    #     self.driver.implicitly_wait(20)
+    #     self.driver.find_element_by_android_uiautomator('text(\"外汇\")').click()   # 外汇
+    #     self.driver.find_element_by_android_uiautomator('text(\"澳元兑日元\")').click()   # 澳元兑日元
+    #     self.driver.find_element_by_android_uiautomator('text(\"买入\")').click()   # 买入澳元兑日元
+    #     self.driver.find_element_by_android_uiautomator('text(\"确认开仓\")').click()  # 确认开仓
+    #     self.driver.find_element_by_id('com.shenzhen.yirabbit:id/viewPendingOrder').click()  #  查看持仓
+    #     self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.support.v4.view.ViewPager/android.widget.LinearLayout/android.widget.ViewAnimator/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.LinearLayout").click()
+    #     self.driver.find_element_by_android_uiautomator('text(\"平仓\")').click()
+    #     self.driver.find_element_by_id('com.shenzhen.yirabbit:id/sureOpenButton').click()
+    #     self.driver.find_element_by_android_uiautomator('text(\"查看持仓\")').click()  # 查看持仓
+
+    # def test_market_sell(self):
+    #     """
+    #     市价卖出澳元兑日元后平仓
+    #     :return:
+    #     """
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/closeImage").click()  # 关闭活动弹窗
+    #     self.driver.implicitly_wait(10)
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/priceRadioButton").click()  # 交易
+    #     self.driver.find_element_by_id("com.shenzhen.yirabbit:id/placeOrderButton").click()  # 去下单
+    #     self.driver.implicitly_wait(20)
+    #     self.driver.find_element_by_android_uiautomator('text(\"外汇\")').click()  # 外汇
+    #     self.driver.find_element_by_android_uiautomator('text(\"澳元兑日元\")').click()  # 澳元兑日元
+    #     self.driver.find_element_by_android_uiautomator('text(\"卖出\")').click()  # 卖出澳元兑日元
+    #     self.driver.find_element_by_android_uiautomator('text(\"确认开仓\")').click()  # 确认开仓
+    #     self.driver.find_element_by_id('com.shenzhen.yirabbit:id/viewPendingOrder').click()  # 查看持仓
+    #     self.driver.find_element_by_xpath("/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ScrollView/android.widget.LinearLayout/android.support.v4.view.ViewPager/android.widget.LinearLayout/android.widget.ViewAnimator/android.view.ViewGroup/android.support.v7.widget.RecyclerView/android.widget.LinearLayout").click()
+    #     self.driver.find_element_by_android_uiautomator('text(\"平仓\")').click()      #  平仓
+    #     self.driver.find_element_by_id('com.shenzhen.yirabbit:id/sureOpenButton').click()   #  平仓
+    #     self.driver.find_element_by_android_uiautomator('text(\"查看持仓\")').click()  # 查看持仓
+
+    def test_limit_buy(self):
+        """
+        限价买入澳元兑日元后撤单
+        :return:
+        """
+        self.driver.find_element_by_id("com.shenzhen.yirabbit:id/closeImage").click()  # 关闭活动弹窗
+        self.driver.implicitly_wait(10)
+        self.driver.find_element_by_id("com.shenzhen.yirabbit:id/priceRadioButton").click()  # 交易
+        self.driver.find_element_by_id("com.shenzhen.yirabbit:id/placeOrderButton").click()  # 去下单
+        self.driver.implicitly_wait(20)
+        self.driver.find_element_by_android_uiautomator('text(\"外汇\")').click()  # 外汇
+        self.driver.find_element_by_android_uiautomator('text(\"澳元兑日元\")').click()  # 澳元兑日元
+        self.driver.find_element_by_android_uiautomator('text(\"买入\")').click()  # 买入澳元兑日元
+        #self.driver.find_element_by_id('com.shenzhen.yirabbit:id/iv_guadan_arrow').click()  #  挂单
+        self.driver.find_element_by_android_uiautomator('text(\"挂单\")').click()  # 挂单
+        self.driver.find_element_by_android_uiautomator('text(\"限价\")').click()  # 限价
+        self.driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ImageView[1]').click()
+        self.driver.find_element_by_android_uiautomator('text(\"确认挂单\")').click()  # 确认挂单
+        self.driver.find_element_by_android_uiautomator('text(\"查看挂单\")').click()  # 查看挂单
+        self.driver.find_element_by_android_uiautomator('text(\"限价挂单\")').click()  # 限价挂单
+        self.driver.find_element_by_android_uiautomator('text(\"撤单\")').click()  # 限价撤单
+        self.driver.find_element_by_android_uiautomator('text(\"确认\")').click()  # 限价撤单
+
+    def test_limit_sell(self):
+        """
+        限价卖出澳元兑日元后撤单
+        :return:
+        """
+        self.driver.find_element_by_id("com.shenzhen.yirabbit:id/closeImage").click()  # 关闭活动弹窗
+        self.driver.implicitly_wait(10)
+        self.driver.find_element_by_id("com.shenzhen.yirabbit:id/priceRadioButton").click()  # 交易
+        self.driver.find_element_by_id("com.shenzhen.yirabbit:id/placeOrderButton").click()  # 去下单
+        self.driver.implicitly_wait(20)
+        self.driver.find_element_by_android_uiautomator('text(\"外汇\")').click()  # 外汇
+        self.driver.find_element_by_android_uiautomator('text(\"澳元兑日元\")').click()  # 澳元兑日元
+        self.driver.find_element_by_android_uiautomator('text(\"卖出\")').click()  # 卖出澳元兑日元
+        # self.driver.find_element_by_id('com.shenzhen.yirabbit:id/iv_guadan_arrow').click()  #  挂单
+        self.driver.find_element_by_android_uiautomator('text(\"挂单\")').click()  # 挂单
+        self.driver.find_element_by_android_uiautomator('text(\"限价\")').click()  # 限价
+        self.driver.find_element_by_xpath('/hierarchy/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.ScrollView/android.widget.LinearLayout/android.widget.RelativeLayout[2]/android.widget.LinearLayout/android.widget.LinearLayout/android.widget.ImageView[2]').click()
+        self.driver.find_element_by_android_uiautomator('text(\"确认挂单\")').click()  # 确认挂单
+        self.driver.find_element_by_android_uiautomator('text(\"查看挂单\")').click()  # 查看挂单
+        self.driver.find_element_by_android_uiautomator('text(\"限价挂单\")').click()  # 限价挂单
+        self.driver.find_element_by_android_uiautomator('text(\"撤单\")').click()  # 限价撤单
+        self.driver.find_element_by_android_uiautomator('text(\"确认\")').click()  # 限价撤单
 
     def teardown(self):
-        # time.sleep(5)
-        # self.driver.quit()
-        pass
+        time.sleep(5)
+        self.driver.quit()
 
 if __name__ == '__main__':
     pytest.main()
-
 
--- zfx_app_element.py
+++ zfx_app_element.py
@@ -1,13 +1,21 @@
+#from appium.common.exceptions import NoSuchElementException
+
 def is_element_present(driver, how, what):  # 定义一个判断元素是否存在的函数，传3个参数
+
+    print('start is_element_present ===>>>>')
+
     apppresent = False
     try:  # 正常结果
         driver = driver.find_element(by = how, value = what)
         if driver.is_displayed():   #   如果元素出现
             apppresent = True
-        else:
-            apppresent = False
+            print("找到当前元素")
     except NoSuchElementException as e:    #  错误结果
         apppresent = False
+        print(e)
+
+    print('end is_element_present ===>>>>')
+
     return apppresent
-    assertTrue(apppresent, msg = "登陆失败")
-    # assertFalse(apppresent, msg = "登陆失败")
+
+#assertFalse(apppresent, msg = "登陆失败")
